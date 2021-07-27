1dc9eb82913998b680dc6bb8521536173e341b3e
Richard Baldin <richardbaldin@gmail.com>
2021-07-26 21:53:30 -0300

    Currying

---
+++ funcional/currying_1_v3.js
@@ -0,0 +1,36 @@
+function textoComTamanhoEntre(min) {
+    return function (max) {
+        return function (erro) {
+            return function (texto) {
+                // Lazy evaluation
+                const tamanho = (texto || '').trim().length
+
+                if(tamanho < min || tamanho > max) {
+                    throw erro
+                }
+
+            }
+        }
+    }
+}
+
+function apicarValidacao(fn, valor) {
+  return function (valor) {
+      //Lazy evaluetion
+      try {
+          fn(valor)
+      } catch (e) {
+          return {error: e}
+      }
+  }
+}
+
+const forcarTamanhoPadrao = textoComTamanhoEntre(4) (255)
+const forcarNomeProdutoValido = forcarTamanhoPadrao('Nome inválido')
+const validarNomeProduto = apicarValidacao(forcarNomeProdutoValido)
+
+const p1 = {nome : 'A', preco: 14.99, desc: 0.25}
+const p2 = {nome : 'AB', preco: 15.00, desc: 0.75}
+
+console.log(validarNomeProduto(p1.nome))
+console.log(validarNomeProduto(p2.nome))
